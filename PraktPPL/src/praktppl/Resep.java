85f11aac18a51eb325436d0f3703cf1a32bb2c84
franciscatherine <72638352+franciscatherine@users.noreply.github.com>
2020-11-09 14:17:41 +0700

    Update Resep.java

--- PraktPPL/src/praktppl/Resep.java
+++ PraktPPL/src/praktppl/Resep.java
@@ -11,13 +11,17 @@ package praktppl;
  * @author Aryasakti Wirasena
  */
 public class Resep {
-    private String idResep;
-    private String[] daftarObat;
+    private String resepID;
+    private int jumlahObat;
+    private String[] namaObat;
+    private int penggunaanObat;
     private String status;
     
-    public Resep(String id, String[] daftar, String stat){
-        idResep = id;
-        daftarObat = daftar;
+    public Resep(String id, int jumlah, String[] nama, int penggunaan, String stat){
+        resepID = id;
+        jumlahObat = jumlah;
+        namaObat = nama;
+        penggunaanObat = penggunaan;
         status = stat;
     }
 }
